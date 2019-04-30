e7a90c3a88999d94f8354f626b9fc46bb114f64b
renhaiwei <me@renhaiwei.cn>
2019-04-30 15:05:24 +0800

    blog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,6 @@
-module.exports = {
+﻿module.exports = {
     title: '任海伟-努力活的像个人',
     description: '在最好的时光里，做最好的自己!',
-    base: '/new/',
     markdown: {
         lineNumbers: true // 显示行号
     }
